b254e883ffb8da9064d9d057b1e043ea2e2ca2b3
MiHente <118524924+MiHente@users.noreply.github.com>
2022-12-10 23:41:55 +0100

    Add files via upload

---
+++ JS/funciones.js
@@ -0,0 +1,51 @@
+/*******************************************************************************************/
+/* Web del romanticismo                                                                    */
+/* Funciones JavaScript para para index.html                                               */
+/* Autor Alejandro Barnestein Martos                                                       */
+/* Versión 1.0                                                                             */
+/* 11 noviembre 2022                                                                       */
+/*******************************************************************************************/
+
+
+
+/********** JS QUE REGULA LA BARRA DE NAVEGACIÓN *******************************************/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+/*GALLERY */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
